73f7d697a933c0d5162752afed8fe35f9a1d77d3
Guillem Hernández Sola <guhernandez@hotelbeds.com>
2017-04-05 09:45:59 +0200

    SauceLabs example finished

--- 06-Saucelabs-Example.groovy
+++ 06-Saucelabs-Example.groovy
@@ -5,12 +5,7 @@ pipeline {
         jdk "java_jenkins"
     }
     environment {
-        saucelabsCredentialId = '8b819d9d-0298-4f8f-9430-6d44e87ac7b5'
-        SAUCE_ACCESS = credentials('1e97caff-ff8e-42b9-95b0-76f1c98fcf38')
-        SAUCE_ACCESS_USR = 'selgp'
-        SAUCE_ACCESS_PSW = '8b819d9d-0298-4f8f-9430-6d44e87ac7b5'
-        SAUCE_USERNAME = 'selgp'
-        SAUCE_ACCESS_KEY = '8b819d9d-0298-4f8f-9430-6d44e87ac7b5'
+        SAUCE_ACCESS = credentials('1e97caff-ff8e-42b9-95b0-76f1c98fcf38') //SauceLabs Credentials via Jenkins Credentials
     }
     stages {
         stage('Checkout') {
@@ -20,7 +15,6 @@ pipeline {
         }
         stage('Test') {
             steps {
-                sh 'source /Users/guillemhernandezsola/.profile'
                 sauce('1e97caff-ff8e-42b9-95b0-76f1c98fcf38') {
                     sh 'mvn -Dtest=WebDriverTest -Dbrowser=chrome -Dhub=http://selgp:8b819d9d-0298-4f8f-9430-6d44e87ac7b5@ondemand.saucelabs.com:80/wd/hub test'
                 }
