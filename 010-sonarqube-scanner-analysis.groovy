326a5f51e1e71d7f284a75381fe37a24ea7f9e75
Guillem Hernandez Sola <guillem@itnove.com>
2018-06-27 11:15:03 +0200

    added sonarqube

---
+++ 010-sonarqube-scanner-analysis.groovy
@@ -0,0 +1,26 @@
+pipeline {
+ agent any
+ tools {
+  maven "maven_354"
+  jdk "java_8"
+ }
+ stages {
+  stage('SCM') {
+   steps {
+    git 'https://github.com/jenkins-docs/simple-java-maven-app.git'
+   }
+  }
+  stage('maven compile') {
+   steps {
+    sh 'mvn install'
+   }
+  }
+  stage('SonarQube Analysis') {
+   steps {
+    withSonarQubeEnv('Sonar') {
+     sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar -Dsonar.projectKey=com.itnove.example -Dsonar.projectName=com.itnove.example -Dsonar.projectVersion=${BUILD_NUMBER} -Dsonar.language=java -Dsonar.sources=src/ -Dsonar.sourcesEnconding=UTF-8 -Dsonar.java.binaries=target/classes -Dsonar.exclusions=src/test/**'
+    }
+   }
+  }
+ }
+}
