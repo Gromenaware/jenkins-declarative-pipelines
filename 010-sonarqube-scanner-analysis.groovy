6a9541e9ecdae48329c2120bf32e86f6cee45904
Guillem Hernandez Sola <guillem@itnove.com>
2020-10-15 13:14:38 +0200

    Added Sonarqube tool for pipelines

--- 010-sonarqube-scanner-analysis.groovy
+++ 010-sonarqube-scanner-analysis.groovy
@@ -16,6 +16,7 @@ pipeline {
   }
   stage('SonarQube Analysis') {
    steps {
+    def scannerHome = tool 'Sonar';
     withSonarQubeEnv('Sonar') {
      sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar -Dsonar.projectKey=com.itnove.example -Dsonar.projectName=com.itnove.example -Dsonar.projectVersion=${BUILD_NUMBER} -Dsonar.language=java -Dsonar.sources=src/ -Dsonar.sourcesEnconding=UTF-8 -Dsonar.java.binaries=target/classes -Dsonar.exclusions=src/test/**'
     }
