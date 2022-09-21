46e17b0158a85db290eebfa5b88c73d768f04a0e
Guillem Hernandez Sola <guillem@agile611.com>
2022-09-21 11:32:45 +0200

    Added Parametrized Scheduler

---
+++ 012-parametrized-scheduler.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+    parameters {
+      string(name: 'PLANET', defaultValue: 'Earth', description: 'Which planet are we on?')
+      string(name: 'GREETING', defaultValue: 'Hello', description: 'How shall we greet?')
+      string(name: 'HOLIDAYS', defaultValue: 'Holidays', description: 'When are you on holidays?')
+    }
+    triggers {
+        parameterizedCron('''
+            # leave spaces where you want them around the parameters. They'll be trimmed.
+            # we let the build run with the default name
+            */1 * * * * %HOLIDAYS=Mallorca;
+            */2 * * * * %GREETING=Hola;PLANET=Pluto
+            */3 * * * * %PLANET=Mars
+        ''')
+    }
+    stages {
+        stage('Example') {
+            steps {
+                echo "${params.GREETING} ${params.PLANET}"
+                script { currentBuild.description = "${params.HOLIDAYS} ${params.GREETING} ${params.PLANET}" }
+            }
+        }
+    }
+}
